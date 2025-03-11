b6154f13b32094f0dac9b1d57254452cb2912508
robis <robin.simonneau@etudiant.univ-reims.fr>
2025-03-11 18:42:52 +0100

    Ajout des triggers (faire une review)

---
+++ trigger.sql
@@ -0,0 +1,45 @@
+-- TODO: a voir si on garde sachant qu'on peut faire un on delete cascade et que ça marche mieux
+CREATE OR REPLACE TRIGGER TRG_delete_device
+BEFORE DELETE ON DEVICE
+FOR EACH ROW
+BEGIN
+    DELETE FROM LICENCE_DEVICE WHERE device_id = :OLD.id;
+    DELETE FROM VM WHERE id = :OLD.id;
+END;
+
+-- TODO:same qu'au dessus
+CREATE OR REPLACE TRIGGER TRG_delete_project
+BEFORE DELETE ON PROJECT
+FOR EACH ROW
+BEGIN
+    DELETE FROM TICKET WHERE id_project = :OLD.id;
+END;
+
+-- TODO:Pour la 3e revoir parce que ça n'a pas de sens
+
+CREATE OR REPLACE TRIGGER TRG_before_insert_licence
+BEFORE INSERT ON LICENCE_DEVICE
+FOR EACH ROW
+BEGIN
+    IF :NEW.expiration_date < :NEW.buying_date THEN
+        RAISE_APPLICATION_ERROR(-20001, 'La date d''expiration doit être supérieure à la date d''achat');
+    END IF;
+    IF :NEW.expiration_date < SYSDATE THEN
+        RAISE_APPLICATION_ERROR(-20002, 'La date d''expiration doit être supérieure à la date actuelle');
+    END IF;
+END;
+
+CREATE OR REPLACE TRIGGER TRG_before_insert_affecation
+BEFORE INSERT ON AFFECTATION
+FOR EACH ROW
+BEGIN
+    :NEW.date_affectation := SYSDATE;   
+END;
+
+CREATE OR REPLACE TRIGGER TRG_before_insert_project
+BEFORE INSERT ON PROJECT
+FOR EACH ROW
+BEGIN
+    :NEW.creation_date := SYSDATE;
+END;
+
